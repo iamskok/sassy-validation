ce489eb3f9b1eb903b363c41c1b0983b98bc8877
Vladimir Skok <skoksworks@gmail.com>
2015-04-13 22:35:10 -0700

    fix sass, gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,45 +8,15 @@ var gulp 			= require('gulp'),
 
 
 gulp.task('default', function() {
-	return gulp.src('_sassy-validation.scss')
+	return gulp.src('sassy-validation.scss')
     	.pipe(sass({
     		style: 'expanded',
     		precision: 10,
         	lineNumbers : true
     	}))
-    	.pipe(gulp.dest('build/'))
-});
-
-/**
-*
-* Default sassDoc task
-* Config options - http://sassdoc.com/gulp/
-* API - http://sassdoc.com/annotations/
-*
-**/
-
-gulp.task('sassdocs', function () {
-  	return gulp.src('sass/**/*.scss')
-	    .pipe(sassdoc({
-	      	'dest': '/sassdocs',
-	      	'verbose': true,
-			'display': {
-				'access': ['public', 'private'],
-				'alias': true
-			},
-	      	'basePath': 'https://github.com/SassySuit/sassy-validation'
-    }));
-});
-
-var options = {
-    cacheDir: 'docs/gh-pages-cache'
-}
-
-gulp.task('deploy', function () {
-    return gulp.src(['./sassdocs/**/*', , '!./node_modules/**'])
-        .pipe(deploy(options));
+    	.pipe(gulp.dest('build'))
 });
 
 gulp.task('watch', function() {
-	gulp.watch('**/*.scss', ['default', 'sassdocs']);
+	gulp.watch('**/*.scss', ['default']);
 });
